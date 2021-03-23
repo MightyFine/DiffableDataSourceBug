af5fa485b2d112e6c3b8a7e1794a6232e5fceb6c
Tim Korotky <tim.korotky@theathletic.com>
2021-03-23 15:23:29 +1100

    Initial Commit

---
+++ DiffableDataSourceDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiffableDataSourceDemo
+//
+//  Created by Tim Korotky on 23/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
